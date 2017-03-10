3922ef413938318fec50afe8bd59b3cd7509ffac
abdul94800 <ah1490@live.mdx.ac.uk>
2017-03-10 17:01:02 +0000

    CMS
    CMS with in line style and view product

--- CMS.php
+++ CMS.php
@@ -1,6 +1,5 @@
 <?php
 //Include the PHP functions to be used on the page 
-
 include('common.php');
 //outputs a BodyTag with onload 
 //Output header and navigation 
@@ -8,7 +7,30 @@ outputHeader("Group 22 Watch Website - CMS");
 outputBodyTag();
 outputBannerNavigation("CMS");
 ?>
+<style> 
+	.grid_element {
+	  width: 200px;
+	  height:200px;
+	}
+	
+	.CMSButton {
+	display: inline-block;
+	float: none;
+	/*font-weight: bold;*/
+    text-align: center;
+    vertical-align: middle;
+    cursor: pointer;
+    border: 0.5px solid;
+    border-color: #ccc;
+    border-radius: 5px;
 
+    color: #333;
+    background-color: #fff;
+    padding: 5px 10px;
+    font-size: 19px;
+    line-height: 1.5;
+	}
+</style>
 <!-- Contents of the page -->
 <div id="content">
     <p>THE CMS</p>
@@ -36,8 +58,19 @@ outputBannerNavigation("CMS");
         <i class="fa fa-plus"></i></span>  Add Product
 		</button> 
 	</form>
-	
-			<br><br>
+	        <br><br>
+			VIEW Product/s
+			<br>
+			<form action="ViewProduct.php" method="post">
+	        <input style="width:400px;height:20px" id="Search" type="text" name="_id" placeholder="Product ID">
+	        <br>
+		    <button type="submit" class="CMSButton" value=""> 	
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+        <i class="fa fa-times"></i></span> View Product
+		</button>
+		</form>
+		
+		<br><br>
 			EDIT Product
 			<br>
 						
@@ -104,8 +137,3 @@ video("Watch");
 outputFooter();
 //Output the footer
 ?>
-
-
-
-
-
