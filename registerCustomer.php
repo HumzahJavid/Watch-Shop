8a46f8f44fdb913fbc9cfb73dcaa988441a23b46
HumzahJavid <HumzahJavid@users.noreply.github.com>
2017-03-14 19:09:32 +0000

    Redirects for customer login/registration
    Added redirects for customer login and registration, for various
    scenarios.

--- registerCustomer.php
+++ registerCustomer.php
@@ -19,26 +19,43 @@ $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
 $number = filter_input(INPUT_POST, 'number', FILTER_SANITIZE_STRING);
 //Extract the data that was sent to the server
 
-$dataArray = [
-	"customerID" => $customerID,
-    "username" => $username, 
-    "email" => $email, 
-    "password" => $password,
-	"number" => $number
- ];
- //Convert to PHP array
+$findCriteria = [ 
+	"email" => $email,
+];
+//Create a PHP array with our search criteria
 
-$returnVal = $collection->insert($dataArray);
-//Add the new product to the database
+$cursor = $db->customers->find($findCriteria);
+//Find all of the customers that match this criteria
 
-if($returnVal['ok']==1){
-    echo 'ok' ;
-}
-else {
-    echo 'Error adding customer';
-}
-//Echo result back to user
+ if($cursor->count() == 0){
+	//if no duplicate emails are found 
+	$dataArray = [
+		"customerID" => $customerID,
+		"username" => $username, 
+		"email" => $email, 
+		"password" => $password,
+		"number" => $number
+	];
+	//Convert to PHP array
 
+	$returnVal = $collection->insert($dataArray);
+	//Add the new product to the database
+	if($returnVal['ok']==1){
+		echo '<h1> Registration successful <br>
+					Redirecting...</h1> ' ;
+		header('Refresh: 3, url = /loginPage.php');
+	} else {
+		echo 'Error adding customer';
+	}
+	//Echo result back to user
+	
+ } else {
+		echo '<h1> Error: Email already exists <br>
+					Redirecting...</h1> ';
+		header('Refresh: 3, url = /registrationPage.php');
+		//If the email already exists, redirect to registration page
+ }
+ 
 $mongoClient->close();
 //Close the connection
 
