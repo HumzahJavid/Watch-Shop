b292d95007b88a3c1fdc186c2a076d55d9d96eb0
HumzahJavid <HumzahJavid@users.noreply.github.com>
2017-03-14 14:17:53 +0000

    deleteProduct method 1
    Exracting all information about a product reinserting that information
    after modifying the quanitity field

--- CMS/deleteProduct.php
+++ CMS/deleteProduct.php
@@ -1,25 +1,46 @@
 <?php
-//needs implementing, of searching for a product to delete
 $mongoClient = new MongoClient();
 //Connect to database
-
 
 $db = $mongoClient->ecommerce;
 //Select a database
 
- 
+$productID= filter_input(INPUT_POST, 'productID', FILTER_SANITIZE_STRING);
+$deleteQuantity = filter_input(INPUT_POST, 'quantity', FILTER_SANITIZE_STRING);
 
-$productID= filter_input(INPUT_POST, 'ID', FILTER_SANITIZE_STRING);
-$productQuantity = filter_input(INPUT_POST, 'quantity', FILTER_SANITIZE_STRING);
+$findCriteria = [
+    "productID" => $productID, 
+ ];
+//Create a PHP array with our search criteria
 
-$removeProduct = [
-        "ID" => $productID,
-        "quantity" => $productQuantity		
-];
+$cursor = $db->products->find($findCriteria);
+//Find all of the customers that match this criteria
 
-var_dump($removeProduct);
+foreach ($cursor as $field){
+$ID= $field['_id'];
+$productName = $field['name'];
+$productPrice = $field['price'];
+$oldProductQuantity = ($oldProductQuantity - $deleteQuantity);
+$url = $field['url'];
+$productKeyword = $field['keyword'];
+}
+//get all the information, reinsert it into mongoDB with different field for quantity.
 
-$Val = $db->product->remove($removeProduct);
+$productQuantity = ($oldProductQuantity - $deleteQuantity);
+//calculate the new product quantity
+
+$dataArray = [
+    "_id" => new MongoId($ID), 
+	"productID" => $productID,
+    "name" => $productName, 
+    "price" => $productPrice,
+	"quantity" => $productQuantity,
+	"url" => $url,
+	"keyword" => $productKeyword
+ ];
+ //create product dataArray with a new product quantity
+
+$Val = $db->product->save($dataArray);
 
 if($Val['ok']==1){
 	echo 'Ok ' . $Val['n'] . ' documents deleted.';
@@ -27,9 +48,5 @@ if($Val['ok']==1){
 else{
 	echo 'there is an error please try again';
 }
-
 $mongoClient->close();
-
-
-
 ?>
