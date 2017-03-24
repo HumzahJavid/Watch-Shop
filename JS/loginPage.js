7300e0cc26e587af2bd054bfaf06623ddf351e83
HumzahJavid <HumzahJavid@users.noreply.github.com>
2017-03-24 17:01:07 +0000

    AJAX Login & Registration (mssing changes)
    Missed some changes for Login for the previous commit

--- JS/loginPage.js
+++ JS/loginPage.js
@@ -1,47 +1,33 @@
+var email = document.getElementById("emailInput").value;
 function login() {
-    var enteredUsername = document.getElementById("usernameInput").value;
-    var enteredPassword = document.getElementById("passwordInput").value;
-    var enteredEmail = document.getElementById("emailInput").value;
-    var error = document.getElementById("loginFailure");
-    blankFields(enteredUsername, enteredPassword, enteredEmail);
-    //get the user account as an object
-   
-    if ((userAccount.username === enteredUsername) && (userAccount.password === enteredPassword)) {
-        updateBanner(); //from banner.js
-    } else {
-        error.innerHTML = "username/password incorrect";
-    }
-}
-
-function blankFields(username, password, email) {
-    var userError = document.getElementById("usernameError");
-    var hiddenUserError = document.getElementById("usernameError2");
-    var passError = document.getElementById("passwordError");
-    var hiddenPassError = document.getElementById("passwordError2");
-    var emailError = document.getElementById("emailError");
-    var hiddenEmailError = document.getElementById("emailError2");
-    if (username.length === 0) {
-        userError.innerHTML = "*username is missing";
-        hiddenUserError.innerHTML = userError.innerHTML;
-    } else {
-        userError.innerHTML = "";
-        hiddenUserError.innerHTML = userError.innerHTML;
-    }
-
-    if (password.length === 0) {
-        passError.innerHTML = "*password is missing";
-        hiddenPassError.innerHTML = passError.innerHTML;
-    } else {
-        passError.innerHTML = "";
-        hiddenPassError.innerHTML = passError.innerHTML;
-    }
-
-    if (email.length === 0) {
-        emailError.innerHTML = "*email is missing";
-        hiddenEmailError.innerHTML = emailError.innerHTML;
-    } else {
-        emailError.innerHTML = "";
-        hiddenEmailError.innerHTML = emailError.innerHTML;
-    }
+	var request = new XMLHttpRequest();
+			
+	request.onload = function() {
+		
+		if(request.readyState == 4 && request.status == 200) {
+			var responseData =  request.responseText;
+			if(responseData === "ok"){
+                            document.getElementById("loginPara").innerHTML = "<h1> </h1>";
+                            document.getElementById("loginFailure").innerHTML = "";//Clear error messages
+							updateBanner(email);
+                        }
+                        else {
+						document.getElementById("loginFailure").innerHTML = responseData; 
+						}
+						
+			}
+			
+		else {
+			document.getElementById("loginFailure").innerHTML = "Error communicating with server: ";
+		}
+		
+		}
+		var email = document.getElementById("emailInput").value;
+		var Password = document.getElementById("passwordInput").value;
+		
+		request.open("POST", "loginCustomer.php", true);
+		request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+		
+		request.send("email=" + email + "&password=" + Password);
 }
 
--- loginPage.php
+++ loginPage.php
@@ -12,15 +12,13 @@ outputBannerNavigation("Login");
 <div id="content">
     <div id ="loginPara">
         <p>To purchase a product you must be logged in, you will be prompted to login when checking out if you have not done so already</p>
-        <form action="loginCustomer.php" id="loginDetails" method="post">
             Email:<br>
             <span id="emailError2" class="hidden"></span><input type="email" id="emailInput" name="email" title="Enter an email address"><span id="emailError" class="error"></span>
             <br>
             Password:<br>
             <span id="passwordError2" class="hidden"></span><input type="password" id="passwordInput" name="password" title="Enter a password"><span id="passwordError" class="error"></span>
             <br><br>
-            <input type="submit" value="login" title="Login">
-        </form>
+            <button onclick="login()">Login</button>
     </div> 
     <p id ="loginFailure" class="error"></p>
 
