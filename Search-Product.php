d55e0b279bd66629da61d9532910721f05513230
abdul94800 <ah1490@live.mdx.ac.uk>
2017-03-27 14:35:28 +0100

    Search-Product
    update version of search for product including search functionnality and order of product

--- Search-Product.php
+++ Search-Product.php
@@ -14,13 +14,187 @@ $findCriteria = [
  ];
 
  
-$Val = $db->products-> find($findCriteria)->sort(array("price" => 1));
-	
-	
+$Val = $db->products-> find($findCriteria)->sort(array("price" => -1));
 
-try{
+		
+
+//Close the connection
+$mongoClient->close();
+ 
+ ?>
+ 
+<p>
+SORT BY
+<form action="#" method="post">
+<select name="selected">
+<option value="" >Select</option>
+<option value="price(ascending)">Price LOW to HIGH</option>
+<option value="price(descending)">Price HIGH to LOW</option>
+<option value="quantity(ascending)">Quantity LOW to HIGH</option>
+<option value="quantity(descending)">Quantity HIGH to LOW</option>
+<option value="alphabetical(ascending)">A-Z</option>
+<option value="alphabetical(descending)">Z-A</option>
+</select>
+<input type="submit" name="submit" value="Submit">
+</p>
+
+<?php
+
+if (isset($_POST['submit'])) {
+
+$dropDownMenu = $_POST['selected'];
+
+
+echo"$dropDownMenu";
+
+switch($dropDownMenu){
+	case "price(ascending)":
 	
-echo "<h1>Results</h1>";
+	$Val = $db->products-> find($findCriteria)->sort(array("price" => 1));
+	
+	foreach($Val as $pro){
+    echo "<p>";
+    echo "Product name: " . $pro['name'];
+    echo"<br>";
+    echo " Product price: ". $pro['price'];
+    echo"<br>";
+    echo " Product quantity: " . $pro['quantity'];
+    echo"<br>";
+    echo"<br>";
+    echo " Product image: " . $pro['url'];
+    echo"<br>";
+    echo "</p>";
+    echo'----------------------------------------------------';
+    echo'<br>';
+}
+	break;
+	case "price(descending)":
+	
+	$Val = $db->products-> find($findCriteria)->sort(array("price" => -1));
+	
+	foreach($Val as $pro){
+    echo "<p>";
+    echo "Product name: " . $pro['name'];
+    echo"<br>";
+    echo " Product price: ". $pro['price'];
+    echo"<br>";
+    echo " Product quantity: " . $pro['quantity'];
+    echo"<br>";
+    echo"<br>";
+    echo " Product image: " . $pro['url'];
+    echo"<br>";
+    echo "</p>";
+    echo'----------------------------------------------------';
+    echo'<br>';
+}
+	break;
+	case "quantity(ascending)":
+	
+	$Val = $db->products-> find($findCriteria)->sort(array("quantity" => 1));
+	
+	foreach($Val as $pro){
+    echo "<p>";
+    echo "Product name: " . $pro['name'];
+    echo"<br>";
+    echo " Product price: ". $pro['price'];
+    echo"<br>";
+    echo " Product quantity: " . $pro['quantity'];
+    echo"<br>";
+    echo"<br>";
+    echo " Product image: " . $pro['url'];
+    echo"<br>";
+    echo "</p>";
+    echo'----------------------------------------------------';
+    echo'<br>';
+}
+	break;
+	case "quantity(descending)":
+	
+	$Val = $db->products-> find($findCriteria)->sort(array("quantity" => -1));
+	
+	foreach($Val as $pro){
+    echo "<p>";
+    echo "Product name: " . $pro['name'];
+    echo"<br>";
+    echo " Product price: ". $pro['price'];
+    echo"<br>";
+    echo " Product quantity: " . $pro['quantity'];
+    echo"<br>";
+    echo"<br>";
+    echo " Product image: " . $pro['url'];
+    echo"<br>";
+    echo "</p>";
+    echo'----------------------------------------------------';
+    echo'<br>';
+}
+	break;
+	case "alphabetical(ascending)":
+	
+	$Val = $db->products-> find($findCriteria)->sort(array("name" => 1));
+	
+	foreach($Val as $pro){
+    echo "<p>";
+    echo "Product name: " . $pro['name'];
+    echo"<br>";
+    echo " Product price: ". $pro['price'];
+    echo"<br>";
+    echo " Product quantity: " . $pro['quantity'];
+    echo"<br>";
+    echo"<br>";
+    echo " Product image: " . $pro['url'];
+    echo"<br>";
+    echo "</p>";
+    echo'----------------------------------------------------';
+    echo'<br>';
+}
+	break;
+	case "alphabetical(descending)":
+	
+	$Val = $db->products-> find($findCriteria)->sort(array("name" => -1));
+	
+	foreach($Val as $pro){
+    echo "<p>";
+    echo "Product name: " . $pro['name'];
+    echo"<br>";
+    echo " Product price: ". $pro['price'];
+    echo"<br>";
+    echo " Product quantity: " . $pro['quantity'];
+    echo"<br>";
+    echo"<br>";
+    echo " Product image: " . $pro['url'];
+    echo"<br>";
+    echo "</p>";
+    echo'----------------------------------------------------';
+    echo'<br>';
+}
+	break;
+	
+	
+	default:
+	echo"yolo";
+	foreach($Val as $pro){
+   echo "<p>";
+   echo "Product name: " . $pro['name'];
+   echo"<br>";
+   echo " Product price: ". $pro['price'];
+   echo"<br>";
+   echo " Product quantity: " . $pro['quantity'];
+   echo"<br>";
+   echo"<br>";
+   echo " Product image: " . $pro['url'];
+   echo"<br>";
+   echo "</p>";
+   echo'----------------------------------------------------';
+   echo'<br>';
+   
+}
+	   break;
+	
+	
+}
+
+}
+else{
 
 foreach($Val as $pro){
    echo "<p>";
@@ -37,14 +211,5 @@ foreach($Val as $pro){
    echo'----------------------------------------------------';
    echo'<br>';
 }
-
 }
-catch(Exception $e) {
-	
-	echo'Cannot identify the word that you wrote';	
-}
-
-//Close the connection
-$mongoClient->close();
- 
- ?>
+?>
